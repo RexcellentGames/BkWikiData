7e9d5884eeebee1e1c6fbf2675a0e8e826b31d7c
eugodr <egordorichev@gmail.com>
2021-06-03 21:34:48 +0300

    Renamed : -> _, added to the script

--- export_sprites.sh
+++ export_sprites.sh
@@ -1,2 +1,3 @@
 echo "Exporting Sprites..."
-./aseprite -b data/images/items.ase --scale 4 --save-as data/images/{slice}.png
+aseprite -b data/images/items.ase --scale 4 --save-as data/images/{slice}.png
+find data/images -depth -name &apos;*:*&apos; -execdir bash -c &apos;mv -- &quot;$1&quot; &quot;${1//:/_}&quot;&apos; bash {} \;
